84ef4ee89b9c012bcbefa54b1a0484f7b585bb66
Karin Lawrence Brown <alexanja464@gmail.com>
2024-10-05 22:31:21 +0300

    assignmendt complete

--- server.js
+++ server.js
@@ -1,32 +1,17 @@
-// Import some dependencies/ packages 
-
-// HTTP framework for handling requests
 const express = require('express');
-//Instance of express framework
-const app = express(); 
-// DBMS Mysql 
 const mysql = require('mysql2');
-// Cross Origin Resourse Sharing 
-const cors = require('cors');
-// Environment variable doc 
 const dotenv = require('dotenv'); 
 const db = require('./database');
 
-// 
-app.use(express.json());
-app.use(cors());
 dotenv.config(); 
 
 // connection to the database 
 const db = mysql.createConnection({
     user: process.env.DB_USERNAME,
-    host: process.env.DB_HOST,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
-    
 });
 
-// Check if there is a connection 
 db.connect((err) => {
     // If no connection 
     if(err) return console.log("Error connecting to MYSQL");
